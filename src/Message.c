27d825552961a32462fcf067819fdf6f9858d98f
Chol Nhial <chol.nhial.chol@gmail.com>
2013-08-31 02:17:37 +1000

    update Message.c

---
+++ src/Message.c
@@ -0,0 +1,42 @@
+#include <include/msg/Message.h>
+
+
+/***
+ *  void messMessage()
+ *
+ *  Prints a message to the user either on stderr
+    or stdout given the type of message.
+ *
+ *
+ *  Paramaters:
+ *    message type, source, cause, message
+ *
+ *
+ *  Returns:
+ *    none
+ *
+ **/
+void messMessage(MessageType messageType,
+     const char* source,
+     const char* cause,
+     const char* message)
+{
+    switch(messageType) {
+
+        case MESSAGE_ERROR:
+            fprintf(stdout, "[ERROR] (%s %s): %s\n", source, cause, message);
+        break;
+        
+        case MESSAGE_WARNING:
+             fprintf(stdout, "[WARNING] (%s %s): %s\n", source, cause, message);
+        break;
+
+        default:
+             fprintf(stdout, "[ERROR] (uknown source - unknown cause): nothing to say\n");
+ 
+    }
+}
+
+
+
+
