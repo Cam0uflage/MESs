9a6fd1b0e867761b30d97906a34dc5e528aa7f48
Chol Nhial <chol.nhial.chol@gmail.com>
2013-09-12 20:06:38 +1000

    update to Common.c

--- include/mess/com/Common.h
+++ include/mess/com/Common.h
@@ -1,7 +1,5 @@
 #ifndef COMMON_H_
 #define COMMON_H_
-#include <errno.h>
-#include <string.h>
 
 /***
  *  void messGetLastErrorString()
@@ -20,4 +18,24 @@
  *
  **/
 char* messGetLastErrorString();
+
+/***
+ *  void messAllocateMemory()
+ *
+ *  Returns a pointer to a buffer.
+ *  
+ *  
+ *
+ *
+ *  Paramaters:
+ *    The size of the buffer to allocate.
+ *
+ *
+ *  Returns:
+ *    A pointer to the buffer if successfull,
+ *    NULL if there was a problem.
+ *
+ *
+ **/
+void* messAllocateMemory(unsigned int size);
 #endif
--- src/Common.c
+++ src/Common.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
-#include <Stdlib.h>
+#include <stdlib.h>
 #include <errno.h>
 
+#include <com/Common.h>
 
-void mess_error(const char* 
+/***
+ *  void messGetLastErrorString()
+ *
+ *  Returns the last error string set by
+ *  Errno
+ *  
+ *
+ *
+ *  Paramaters:
+ *    none
+ *
+ *
+ *  Returns:
+ *    the error string
+ *
+ **/
+char* messGetLastErrorString()
+{
+    return strerror(errno);
+}
+
+/***
+ *  void messAllocateMemory()
+ *
+ *  Returns a pointer to a buffer.
+ *  
+ *  
+ *
+ *
+ *  Paramaters:
+ *    The size of the buffer to allocate.
+ *
+ *
+ *  Returns:
+ *    A pointer to the buffer if successfull,
+ *    NULL if there was a problem.
+ *
+ *
+ **/
+void* messAllocateMemory(unsigned int size)
+{
+    void* buffer;
+    buffer = malloc(size);
+    if(buffer == NULL) {
+        return NULL;
+    }
+    
+    return buffer;
+}
